6691e6d5afdf770fe169d57c04f2a1d8f0461e86
Galib77 <razagalib7@gmail.com>
2024-05-05 19:58:11 +0530

    components/sections/WhyTsn.js added

---
+++ components/sections/WhyTsn.js
@@ -0,0 +1,213 @@
+
+const WhyTSN = () => {
+  return (
+    <div>
+        <section className="why-container">
+        <div class="y-us-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="y-us-head">
+                        <div class="y-us-title">
+                            <h2 className="text-white">WHY TSN</h2>
+                            <p  className="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim </p>
+                            <span class="y-us-title-border"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="y-us-content">
+                        <div class="service-3">
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-integrated-connectivity.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4  className="text-white">Integrated Connectivity</h4>
+                                        <p className="text-white">A unified customer experience across all channels, seamlessly integrating messaging, social media, and more for cohesive communication strategies</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-incremental-innovation.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Incremental Innovation</h4>
+                                        <p className="text-white">Stay ahead in the digital landscape, where continuous innovation drives our solutions, keeping your brand at the forefront of evolving communication trends</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-security-and-compliance.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Security and Compliance</h4>
+                                        <p className="text-white">We prioritize your data security and ensure compliance with relevant regulations.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-omnichannel-expertise.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Omnichannel Expertise </h4>
+                                        <p className="text-white">We understand the complexities of managing multiple communication channels. Our team has the knowledge and experience to create a cohesive experience across all platforms.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-measureable-roi.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Measurable ROI</h4>
+                                        <p className="text-white">We believe in results. We track key metrics and provide transparent reporting so you can see the tangible impact of our integrated connectivity solutions.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-building-brand-advocacy.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Building Brand Advocacy</h4>
+                                        <p className="text-white">We empower you to turn satisfied customers into brand advocates. We help you cultivate a loyal community by fostering positive interactions and fostering two-way communication.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="why-choose-banner">
+                        <img src="http://themeinnovation.com/demo2/html/build-up/img/home1/why-choose.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="y-us-contentbox">
+                        <div class="service-3">
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-tailored-solutions.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Tailored Solutions</h4>
+                                        <p className="text-white">We tailor our expert strategies specifically to your business requirements, improving customer engagement and maximizing satisfaction levels.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-scaleability-flexibility.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Scalability and Flexibility</h4>
+                                        <p className="text-white">Our solutions can adapt and grow with your business. We offer flexible packages to fit your specific needs and budget.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-24-7-dynamic-support.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">24/7 Dynamic Support</h4>
+                                        <p className="text-white">Your brand never sleeps, and neither do we. Telesourcenow empowered its customers with dynamic support that fuels the brand's every move.    </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-data-driven-strategies.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Data-Driven Strategies </h4>
+                                        <p className="text-white">We don't just guess. We use data analytics to understand your customer behavior and tailor your communication strategy for maximum effectiveness.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-ai-powered-insights.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">AI-Powered Insights</h4>
+                                        <p className="text-white">We harness the power of artificial intelligence to unlock valuable customer data.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service-box">
+                                <div class="clearfix">
+                                    <div class="iconset">
+                                            <img 
+                                                className="img-fluid p-2" 
+                                                style={{ borderRadius: '50px'}}
+                                                src="/images/whytsn-competitive-edge-digital-age.webp" />
+                                    </div>
+                                    <div class="y-us-content">
+                                        <h4 className="text-white">Competitive Edge in the Digital Age</h4>
+                                        <p className="text-white">In today's digital world, standing out is crucial. We equip you with the tools and strategies to stay ahead of the curve, ensuring your brand voice remains fresh, engaging, and competitive.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div> 
+        </section>
+    </div>
+  )
+}
+
+export default WhyTSN
