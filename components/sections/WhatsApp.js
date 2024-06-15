37401c8ccb4d224abeac017388784d7ecb7c8b7e
Galib77 <razagalib7@gmail.com>
2024-06-15 12:32:42 +0530

    welcome message changed

--- components/sections/WhatsApp.js
+++ components/sections/WhatsApp.js
@@ -4,7 +4,7 @@ const WhatsApp = () => {
   return (
     <div class="sticks-icon">
         <a 
-        href="https://wa.me/+918080876443?text=I'm%20interested%20in%20Mark%20Consultant.%20Please%20Share%20me%20more%20details."
+        href="https://wa.me/+918080876443?text=I'm%20interested%20in%20TelesourceNow.%20Please%20Share%20me%20more%20details."
         class="sticks-icon__link" target="_blank">
             <i class="fab fa-whatsapp"></i>
         </a>
